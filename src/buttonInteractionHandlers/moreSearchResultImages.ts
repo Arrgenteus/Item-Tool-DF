2bc833f1866ca1faee92863f323dd0b297db8e19
Arrgenteus <82183605+Arrgenteus@users.noreply.github.com>
2022-06-16 00:09:36 +0530

    Add button to view all images if item has more than one appearance (#36)
    - Add a button to view all images if an item has more than 1 appearance. Replaces the "Other Appearances" section that was present
    - Update/Delete this button as required when similar results are clicked

---
+++ src/buttonInteractionHandlers/moreSearchResultImages.ts
@@ -0,0 +1,32 @@
+import { ButtonInteraction, MessageEmbedOptions } from 'discord.js';
+import { ActionRowInteractionData } from '../eventHandlerTypes';
+import { getAllItemImages } from '../interactionLogic/search/search';
+import {
+    MORE_SEARCH_RESULT_IMAGES_INTERACTION_ID,
+    SearchableItemCategory,
+} from '../interactionLogic/search/types';
+
+const buttonInteration: ActionRowInteractionData = {
+    names: [MORE_SEARCH_RESULT_IMAGES_INTERACTION_ID],
+    preferEphemeralErrorMessage: true,
+    run: async (interaction: ButtonInteraction, args: string[]): Promise<void> => {
+        const [itemName, itemSearchCategory, maxLevelInput, minLevelInput]: string[] = args;
+
+        const maxLevel = maxLevelInput === '' ? undefined : Number(maxLevelInput);
+        const minLevel = minLevelInput === '' ? undefined : Number(minLevelInput);
+
+        const moreSearchResultImagesEmbeds: MessageEmbedOptions[] = await getAllItemImages({
+            itemName,
+            itemSearchCategory: itemSearchCategory as SearchableItemCategory,
+            maxLevel,
+            minLevel,
+        });
+
+        await interaction.reply({
+            embeds: moreSearchResultImagesEmbeds,
+            ephemeral: true,
+        });
+    },
+};
+
+export default buttonInteration;
