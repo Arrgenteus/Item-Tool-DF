91ffb0b226c5ebb84b2e73f2425db23ce2fae7bb
Silvy <silveee98@gmail.com>
2023-03-11 02:47:06 +0530

    Auto-delete bot message responses if user messages are deleted (#43)
    - Delete corresponding bot message response if the user's message which contained the command is deleted. Works for the last 20 commands used and if less than an hour has passed since the command's use.
    - Remove placeholder !sort chat command

---
+++ src/utils/store.ts
@@ -0,0 +1,8 @@
+import { Message, Snowflake } from 'discord.js';
+import LRUCache from 'lru-cache';
+
+// LRU cache which maintains corresponding bot message responses to user messages which contain commands
+export const botResponseCache: LRUCache<Snowflake, Message> = new LRUCache({
+    max: 20,
+    ttl: 1000 * 60 * 60, // Prune after an hour
+});
