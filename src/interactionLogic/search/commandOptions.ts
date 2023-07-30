421841930aba4432165ac668da7c8f5d19db2bd2
Arrgenteus <82183605+Arrgenteus@users.noreply.github.com>
2023-07-30 21:19:06 +0530

    re-add pet command

--- src/interactionLogic/search/commandOptions.ts
+++ src/interactionLogic/search/commandOptions.ts
@@ -19,6 +19,7 @@ export const searchCommandOptions: (SearchableItemCategory | SearchableItemCateg
     'trinket',
     'bracer',
     'cosmetic',
+    'pet',
 ];
 
 export const compareCommandCategoryList: (SearchableItemCategory | SearchableItemCategoryAlias)[] =
